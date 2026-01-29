bd8e6623485618ea1b20b0ea7ec481953cc1e177
Akash T Rathod <akashrathod3565@gmail.com>
2026-01-30 01:09:25 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="GenAI-Chatbot",
+    version="0.1.0",
+    author="Akash Rathod",
+    author_email="akashrathod3565@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+ 
+)
